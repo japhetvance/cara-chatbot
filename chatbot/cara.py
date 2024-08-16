41c0059b531ca4bdbb407c95f4fa5fbef1e01efd
japhetvance <jvmpamonag@gmail.com>
2024-08-17 04:18:52 +0800

    initial commit

---
+++ chatbot/cara.py
@@ -0,0 +1,135 @@
+import streamlit as st
+import os
+import uuid
+from uuid import uuid4
+import re
+import getpass
+import pandas as pd
+import numpy as np
+import nltk
+from langchain_community.retrievers import PineconeHybridSearchRetriever
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import create_history_aware_retriever, create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_community.chat_message_histories import ChatMessageHistory
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.runnables.history import RunnableWithMessageHistory
+from pinecone import Pinecone as PineconeClient, ServerlessSpec
+from pinecone_text.sparse import BM25Encoder
+nltk.download('punkt')
+
+os.environ['PINECONE_API_KEY'] = 'cee81f14-c2d5-4006-a70a-6795fb7f7270'
+os.environ['PINECONE_INDEX_NAME'] = 'caap-chatbot'
+os.environ["OPENAI_API_KEY"] = open("../openaiapikey.txt", "r").read()
+
+st.title("Make Every Aviatiors Life Easier with C.A.R.A Chatbot")
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = [{"role": "assistant", "content": "Hi, I'm Cara, your aviation assistant. How can I assist you today?"}]
+
+# Display or clear chat messages
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+
+def clear_chat_history():
+    st.session_state.messages = [{"role": "assistant", "content": "Hi, I'm Cara, your aviation assistant. How can I assist you today?"}]
+    if "session_id" in st.session_state:
+        st.session_state.pop("session_id")
+st.sidebar.button('Clear Chat History', on_click=clear_chat_history)
+
+# Initialize the retriever and language model
+embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+# Initialize Pinecone
+pc = PineconeClient(api_key=os.getenv('PINECONE_API_KEY'))
+index = pc.Index(os.getenv('PINECONE_INDEX_NAME'))
+bm25_encoder = BM25Encoder().default()
+
+# Create the retriever with the BM25 encoder
+retriever = PineconeHybridSearchRetriever(embeddings=embeddings, sparse_encoder=bm25_encoder, index=index)
+
+llm = ChatOpenAI(model="gpt-4o-mini", temperature=0)
+
+# Contextualize question
+contextualize_q_system_prompt = """Given a chat history and the latest user question \
+which might reference context in the chat history, formulate a standalone question \
+which can be understood without the chat history. Do NOT answer the question, \
+just reformulate it if needed and otherwise return it as is."""
+contextualize_q_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", contextualize_q_system_prompt),
+        MessagesPlaceholder("chat_history"),
+        ("human", "{input}"),
+    ]
+)
+history_aware_retriever = create_history_aware_retriever(
+    llm, retriever, contextualize_q_prompt
+)
+
+# Answer question
+qa_system_prompt = """You are Cara, an AI assistant specializing in aviation queries. \
+Use the provided context to answer the user's question. \
+If the answer is not in context, just say that you don't know and ask to provide more information or ask aviation related queries only \
+But you can provide general information if the question is not tailored on the context. \
+Do not repeatedly ask for more questions or clarifications . \
+Keep your response concise, limited to three sentences if possible. \
+When addressing sensitive queries directly answered by the context, mention "According to PCAR" or a similar phrase, ensuring that "PCAR" is highlighted. But dont mention it for general questions that is not specifically tailored on the context \
+Capitalize all abbreviations you use.
+
+{context}"""
+qa_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", qa_system_prompt),
+        MessagesPlaceholder("chat_history"),
+        ("human", "{input}"),
+    ]
+)
+question_answer_chain = create_stuff_documents_chain(llm, qa_prompt)
+
+rag_chain = create_retrieval_chain(history_aware_retriever, question_answer_chain)
+
+# Statefully manage chat history
+store = {}
+
+def get_session_history(session_id: str = None) -> tuple:
+    if session_id is None:
+        session_id = str(uuid.uuid4())
+    if session_id not in store:
+        store[session_id] = ChatMessageHistory()
+    return session_id, store[session_id]
+
+def get_current_session_history() -> tuple:
+    if "session_id" not in st.session_state or "history" not in st.session_state:
+        st.session_state.session_id, st.session_state.history = get_session_history()
+    return st.session_state.session_id, st.session_state.history
+
+conversational_rag_chain = RunnableWithMessageHistory(
+    rag_chain,
+    lambda: get_current_session_history()[1],
+    input_messages_key="input",
+    history_messages_key="chat_history",
+    output_messages_key="answer",
+)
+
+# Generate a new session ID automatically
+session_id, history = get_current_session_history()
+
+# User-provided prompt
+if prompt := st.chat_input():
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.write(prompt)
+
+# Generate a new response if last message is not from assistant
+if st.session_state.messages[-1]["role"] != "assistant":
+    with st.chat_message("assistant"):
+        with st.spinner("Thinking..."):
+            response = conversational_rag_chain.invoke(
+                {"input": prompt}
+            )["answer"]
+            st.write(response)
+            # st.write("Session ID:", session_id)
+    message = {"role": "assistant", "content": response}
+    st.session_state.messages.append(message)
---
+++ streamlit_app.py
@@ -0,0 +1,147 @@
+import streamlit as st
+
+
+st.set_page_config(layout='wide')
+
+if "role" not in st.session_state:
+    st.session_state.role = None
+
+if "vote" not in st.session_state:
+    st.session_state.vote = None
+ROLES = ["Aviation Expert", "Aviation Enthusiast"]
+
+@st.dialog("❗Key Reminder",width="large")
+def vote(role):
+    st.markdown("""While our app provides valuable information about aviation, it's important to understand that our dataset is specifically limited to the guidelines and regulations set forth by the Philippine Civil Aviation Regulation (PCAR). This means our coverage is restricted to the regulations within this jurisdiction and may not include broader or international aviation standards.
+                <br><br>For comprehensive and up-to-date aviation regulations, please refer to our data sources listed here:""", unsafe_allow_html=True
+               )
+    col1, col2 = st.columns(2)
+    col1.link_button("Civil Aviation Authority of the Philippines", "https://caap.gov.ph/", use_container_width = True)
+    col2.link_button("Philippine Civil Aviation Regulation", "https://caap.gov.ph/civil-aviation-regulations/", use_container_width = True)
+    
+    agree = st.checkbox("I acknowledge that I understand the limitations of the model")
+   
+    if st.button("Try on CARA ChatBot ↗︎", type = "primary"):
+        if agree:
+            st.session_state.vote = {"role": role}
+            st.rerun()
+        else: 
+            st.error("It is important to acknowledge the limitations of the model.")
+            
+def login():
+    col1, col2, col3 = st.columns([1,3,1])
+    
+    col2.image('datas/cara.png')
+    col1.image('datas/caragpt.png')
+
+    col2.header("Hello, C.A.R.A!")
+    content = """
+    Welcome to C.A.R.A., your trusted aviation assistant designed to provide you with real-time insights and guidance on the regulations set by the Civil Aviation Authority of the Philippines. 
+    AI at its core, C.A.R.A. ensures you have quick and easy access to essential aviation information whenever you need it. 
+    Whether you're an aviation professional, student, or enthusiast, C.A.R.A. is here to support your journey with accurate and reliable answers, making your experience smoother and more informed.
+    Discover a smarter way to navigate the skies with C.A.R.A. by your side.<br><br>
+
+    """
+    col2.markdown(content, unsafe_allow_html=True)
+
+    col2.subheader("Begin Your Journey")
+    col2.markdown("Choose your profile below to tailor your experience:")
+
+    role = col2.radio("I am a ",ROLES, index = None, label_visibility = "collapsed",captions = ["Empowering you with comprehensive aviation knowledge, tools, and resources to excel in your career and stay informed on the latest regulations.", "Providing you with easy access to accurate aviation information, insights, and updates to fuel your passion for flying."] )
+    # role = col2.selectbox("Choose your role", ROLES)
+    if st.session_state.vote == None: 
+        
+        if col2.button("Next"):
+            if role:
+                vote(role)
+            else:
+                col2.error("Please Select Your Profile in order to proceed.")
+    else:
+        st.session_state.role = st.session_state.vote['role']
+
+        st.rerun()
+        
+def logout():
+    # st.session_state = None
+    st.session_state.role = None
+    st.session_state.vote = None
+    st.rerun()
+    
+def contactus():
+    col1, col2 = st.columns(2)
+    col1.title('The team behind C.A.R.A.')
+    col1.subheader("ABOUT US")
+    contactinfo = """
+    C.A.R.A. (Flying J's most advanced aviation assistant system) is your trusted digital companion in the aviation industry.
+    We are committed to providing accurate, up-to-date information on Philippine Civil Aviation Regulations (PCAR) to aviation professionals, students, and enthusiasts alike.
+    Our platform leverages advanced AI technology to deliver real-time insights and guidance, simplifying the complexities of aviation regulations and ensuring compliance and safety.
+    At C.A.R.A., we believe in making aviation knowledge accessible and easy to understand, empowering you to excel in your aviation journey.
+    Whether you’re navigating airworthiness standards or staying informed on personnel licensing requirements, C.A.R.A. is here to support you every step of the way.
+    """
+    col1.markdown(contactinfo, unsafe_allow_html=True)
+    col1.subheader("MISSION")
+    contactinfo = """
+    To empower the aviation community by providing accurate, real-time information and guidance on Philippine Civil Aviation Regulations. 
+    C.A.R.A. is dedicated to enhancing safety, compliance, and understanding within the industry, ensuring that every aviator, student, and enthusiast has the knowledge they need to navigate the skies with confidence.
+    """
+    col1.markdown(contactinfo, unsafe_allow_html=True)
+    col1.subheader("VISION")
+    contactinfo = """
+    To be the leading digital assistant in aviation, setting the standard for accessibility and reliability in aviation information. C.A.R.A. envisions a future where every aviation professional and enthusiast is equipped with the tools and insights necessary to achieve excellence in their field, fostering a safer and more informed aviation community.
+    """
+    col1.markdown(contactinfo, unsafe_allow_html=True)
+    
+def medinfohubplus():
+    st.markdown(f"<h1 style='text-align: center;'>Greetings from C.A.R.A, {role} 👨‍✈️</h1>", unsafe_allow_html=True)
+    st.divider()
+    st.markdown("<h4 style='text-align: center;color: #75C2F6;'><b><i>C.A.R.A</b></i><i> is your ultimate resource for accessible, reliable, and comprehensive aviation information. Our platform is designed to enhance your understanding of aviation regulations, support industry compliance, and simplify complex guidelines. With real-time insights and guidance, C.A.R.A. ensures you stay informed and up-to-date with the latest aviation standards.</i></h4>", unsafe_allow_html=True)
+    st.divider()
+    col1, col2 = st.columns(2)
+    with col1:
+        st.subheader("💬CARA ChatBot")
+        st.markdown("***Empowering You with Reliable Aviation Information***")
+        st.markdown("C.A.R.A. leverages advanced AI to deliver accurate and easy-to-understand aviation guidelines and regulations. Our goal is to make aviation information accessible to everyone, enhancing your understanding and supporting informed decision-making. By providing real-time insights tailored to your needs, C.A.R.A. ensures you stay updated with the latest industry standards. Trust C.A.R.A. to be your guide in navigating the complexities of aviation regulations with confidence.")
+        
+
+    with col2:
+        st.subheader("✈️Philippine Civil Aviation Regulation")
+        st.markdown("***Providing Comprehensive Guidelines and Regulations for Philippine Aviation***")
+        st.markdown("The Civil Aviation Regulations (CAR) outline the regulatory requirements for aviation safety, including aircraft operations, airworthiness, and personnel licensing. These regulations are organized into various Parts, each addressing specific aspects of aviation safety and compliance. Each Part is designed to ensure that all operational, maintenance, and personnel standards are met, providing a comprehensive framework for safe and efficient aviation practices.")
+        
+    col3, col4 = st.columns(2)
+    if col3.button('Try on CARA Chatbot ↗︎', type = "primary", use_container_width = True):
+        st.switch_page("chatbot/cara.py")
+    col4.link_button("Explore the data source here 🛪", "https://caap.gov.ph/civil-aviation-regulations/", use_container_width = True)
+
+role = st.session_state.role
+
+logout_page = st.Page(logout, title="End Session", icon=":material/logout:")
+about_us = st.Page(contactus, title="Contact Us", icon="✉️")
+medinfohubplus_info = st.Page(medinfohubplus, title="About Our Data App", icon="📱", default=(role == role))
+
+chatbot = st.Page(
+    "chatbot/cara.py",
+    title="Chat with C.A.R.A",
+    icon="💬",
+)
+about_us_pages = [medinfohubplus_info,about_us]
+account_pages = [logout_page]
+data_apps = [chatbot]
+
+st.logo(
+    "datas/everyday.png",
+    icon_image= "datas/cara-logo.png",
+)
+
+page_dict = {}
+if st.session_state.role in ["Aviation Expert", "Aviation Enthusiast", "Neither"]:
+    page_dict["Application"] = data_apps
+if st.session_state.role in ["Aviation Expert", "Aviation Enthusiast", "Neither"]:
+    page_dict["The Team"] = about_us_pages
+
+if len(page_dict) > 0:
+    pg = st.navigation(page_dict | {"Session": account_pages})
+else:
+    pg = st.navigation([st.Page(login)]) #defaults to login page if no acceptable role is selected
+
+pg.run()
